4afa25f52336a8137142e90d30cc93b039335bdc
Golo Roden <golo.roden@thenativeweb.io>
2018-06-15 16:24:45 +0200

    Introduce view object.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@
 
 const wolkenkit = require('wolkenkit-client');
 
-const render = function (messages) {
-  const html = messages.map(message =>
-    `<li class="message">
-      <div class="label">${message.text}</div>
-      <div class="likes" data-message-id="${message.id}">
-        <span class="button">👍</span>
-        <span class="count">${message.likes || 0}</span>
-      </div>
-    </li>`
-  ).join('');
+const view = {
+  messages: document.querySelector('.messages'),
+  newMessage: document.querySelector('.new-message'),
+  sendMessageForm: document.querySelector('.send-message-form'),
 
-  document.querySelector('.messages').innerHTML = html;
+  render (messages) {
+    const html = messages.map(message =>
+      `<li class="message">
+        <div class="label">${message.text}</div>
+        <div class="likes" data-message-id="${message.id}">
+          <span class="button">👍</span>
+          <span class="count">${message.likes || 0}</span>
+        </div>
+      </li>`
+    ).join('');
+
+    view.messages.innerHTML = html;
+  }
 };
 
 const run = async function () {
